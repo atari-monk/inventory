55cd409a191a20d7682ef76416b4523cce3dad97
atari-monk <atari.monk1@gmail.com>
2023-05-10 17:33:36 +0200

    fix bug, upd data

--- form-server/app.js
+++ form-server/app.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 
 // handle the form submission
 app.post("/", (req, res) => {
-	const data = {
+	const newItem = {
 		id: req.body.id,
 		name: req.body.name,
 		description: req.body.description,
